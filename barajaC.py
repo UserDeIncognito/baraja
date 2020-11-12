4157fbe12a3257e2f445ab1a46307e936f3f58a1
carlos lagar <charly_lagar@hotmail.com>
2020-11-12 22:23:59 +0100

    Baraja con jugadores

--- barajaC.py
+++ barajaC.py
@@ -1,8 +1,21 @@
-import baraja
+import Baraja
 
-class baraja():
+class Mibaraja():
 
     def __init__(self,palos,numeros):
         self.palos = palos
         self.numeros = numeros
-        self.mazacote = 
+        self.mazacote = Baraja.creaBaraja(palos, numeros)
+
+    def barajar(self):
+        Baraja.barajar(self.mazacote)
+
+    def repartir(self, num_jugadores, num_cartas):
+        jugadores = []
+        for i in range (num_jugadores):
+            jugadores.append([])
+
+        for carta in range(num_cartas):
+            for jugador in range(num_jugadores):
+                jugadores[jugador].append(self.mazacote.pop(0))
+        return jugadores
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import baraja
+import barajaC
 
-palos = ["corazaones" "picas" "diamantes" , "treboles"] 
-numeros = ["A", "1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+palos = ["Oros", "Copas", "Espadas" , "Bastos"] 
+numeros = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
-ordenada = baraja.creaBaraja(palos, numeros)
-print("Esta es la primera baraja: ", ordenada)
+miBaraja = barajaC.Mibaraja(palos, numeros)
 
-otraBaraja = baraja.creaBaraja(palos, numeros)
-print("esta es la segunda baraja nada mas crearla:", otraBaraja)
-baraja.baraja(otraBaraja)
-print("y ahora la he barajado:", otraBaraja)
-print("Para que fernando se lo crea, la baraja primera:", ordenado)
+print(miBaraja.mazacote)
+
+print(miBaraja.repartir(3,5))
+
+miBaraja.barajar()
+
+print(miBaraja.mazacote)
